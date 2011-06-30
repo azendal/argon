3e5bdb000148429bc7bd30ad43d1dc5cc7bd49c5
azendal <trasvina@gmail.com>
2011-06-30 00:00:21 -0500

    included static file serving to get all static js and html files

--- spec/assets/http/server.js
+++ spec/assets/http/server.js
@@ -19,8 +19,23 @@ var app = {
         },
         
         'POST' : function(req, res) {
-            var content = [1,2,3,4,5,6];
-            respondWith(res, JSON.stringify(content));
+            var data = "";
+
+            req.on("data", function(chunk) {
+                data += chunk;
+            });
+
+            req.on("end", function() {
+                util.log("raw: " + data);
+
+                var json = qs.parse(data);
+
+                util.log("json: " + json);
+                
+                var content = [1,2,3,4,5,6];
+                respondWith(res, JSON.stringify(content));
+                
+            });
         },
         
         'PUT' : function(req, res) {
@@ -45,8 +60,40 @@ var processRequest = function(req, res) {
     console.log('request method', req.method);
     console.log('--------------------------------------');
     
-    
-    app[req.url][req.method](req, res);
+    var uri      = url.parse(req.url).pathname, 
+        filename = path.join(process.cwd(), uri);
+
+    path.exists(filename, function(exists) {
+        if(!exists) {
+            
+            if(app[req.url] && app[req.url][req.method]){
+                app[req.url][req.method](req, res);
+                return;
+            }
+            
+            res.writeHead(404, {"Content-Type": "text/plain"});
+            res.write("404 Not Found\n");
+            res.end();
+            return;
+        }
+
+        if (fs.statSync(filename).isDirectory()) {
+            filename += '/index.html';
+        }
+
+        fs.readFile(filename, "binary", function(err, file) {
+            if(err) {
+                res.writeHead(500, {"Content-Type": "text/plain"});
+                res.write(err + "\n");
+                res.end();
+                return;
+            }
+
+            res.writeHead(200);
+            res.write(file, "binary");
+            res.end();
+        });
+    });
 };
 
 var respondWith = function(res, content){
