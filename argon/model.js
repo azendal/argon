ecc1c8e6a05df4c3553660f45bc731f266594896
Gustavo Ortiz <guspidey@gmail.com>
2011-07-07 13:28:21 -0500

    Add validation support

--- argon/model.js
+++ argon/model.js
@@ -1,8 +1,9 @@
-Module(Argon, 'Model').includes(CustomEventSupport)({
+Module(Argon, 'Model').includes(CustomEventSupport, ValidationSupport)({
     storage : null,
     create  : function (data) {
         this.dispatch('beforeCreate');
         model = new this(data);
+        model.save();
         this.dispatch('afterCreate');
         return model;
     },
@@ -22,28 +23,46 @@ Module(Argon, 'Model').includes(CustomEventSupport)({
                 callback(data);
             }
         });
-    },
 
+        return this;
+    },
+    all : function (callback) {
+      this.read({}, callback);
+      return this;
+    },
+    find : function (id, callback) {
+      this.read({conditions : {id : id}}, callback);
+      return this;
+    },
+    find_by : function (attribute, value, callback) {
+      var customConditions = {};
+      customConditions[attribute] = value;
+
+      this.read({conditions : customConditions}, callback);
+
+      return this;
+    },
     prototype : {
+        errors           : [],
         init             : function (properties) {
             var property;
-            
+
             this.eventListeners = [];
-            
+
             for (property in properties) {
                 if (properties.hasOwnProperty(property)) {
                     this[property] = properties[property];
                 }
             }
-            
+
             return this;
         },
         getProperty      : function (property) {
-          return this[attribute];  
+          return this[attribute];
         },
         setProperty      : function (property, newValue) {
             var originalValue;
-            
+
             if (newValue != originalValue) {
                 originalValue = this[property];
                 this[property] = newValue;
@@ -53,26 +72,29 @@ Module(Argon, 'Model').includes(CustomEventSupport)({
                     newValue      : newValue
                 });
             }
-            
+
             return this;
         },
         updateProperties : function (properties) {
             var property;
-            
+
             for (property in properties) {
                 if (properties.hasOwnProperty(property)) {
                     this.setProperty(property, properties[property]);
                 }
             }
-            
+
             return this;
-        }, 
+        },
         save             : function (callback) {
             var model;
 
             model = this;
 
             this.dispatch('beforeSave');
+            if (!this.isValid()) {
+                return model;
+            }
 
             if (this.hasOwnProperty('id') && this.id !== '') {
                 this.constructor.storage.put(this, function(data){
@@ -90,7 +112,7 @@ Module(Argon, 'Model').includes(CustomEventSupport)({
                     }
                 });
             }
-        }, 
+        },
         destroy          : function (callback) {
             var model = this;
             this.dispatch('beforeDestroy');
@@ -107,3 +129,4 @@ Module(Argon, 'Model').includes(CustomEventSupport)({
         }
     }
 });
+
