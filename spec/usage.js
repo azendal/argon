1dc1b2195e622f297ac21042528e22c6f4846027
azendal <trasvina@gmail.com>
2011-07-24 23:28:55 -0500

    improve argon usage

--- spec/usage.js
+++ spec/usage.js
@@ -7,14 +7,33 @@ Te.suite('Argon Usage')(function() {
         Class(Scope, 'User').includes(Argon.Model)({
             storage : (new Argon.Storage.Local()),
             
+            validations : {
+                username : {
+                    validate : function(){
+                        return this.hasOwnProperty('username') && this.username.length > 0;
+                    },
+                    message : 'username is required'
+                },
+                
+                password : {
+                    validate : function(){
+                        return this.hasOwnProperty('password') && this.password.length > 5;
+                    },
+                    message : 'password is required'
+                }
+            }
         });
         
         Scope.User.create({username:'fernando', password:'123456'}, function(user){
             spec.assert(user.errors.length).toEqual(0);
             spec.assert(user.username).toEqual('fernando');
             spec.assert(user.password).toEqual('123456');
-            console.log(user);
-            spec.completed();
+            
+            user.password = '1234567';
+            user.save(function(user){
+                spec.assert(user.password).toEqual('1234567');
+                spec.completed();
+            });
         });
         
         
