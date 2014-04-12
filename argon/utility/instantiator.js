58ad94b1343ada0bb6f5637ea4f3e53f22def905
azendal <trasvina@gmail.com>
2014-04-12 13:25:59 -0500

    improve documentation

--- argon/utility/instantiator.js
+++ argon/utility/instantiator.js
@@ -1,13 +1,37 @@
+/**
+Provides Instantiation of JavaScript objects with the camel case notation
+@class Instantiator
+**/
 Class("Instantiator")({
   prototype : {
 
+    /**
+    Provides a way to tell the instantiate process where
+    the model classes are located on the system
+    @property classNamespace <public, static> [Object]
+    **/
     classNamespace : {},
 
+    /**
+    Initialization function
+    @property init <public, static> [Function]
+    @argument config <required> [Object] {undefined} configuration object
+    **/
     init : function init(config) {
       this.classNamespace = config.classNamespace || window;
       return this;
     },
 
+    /**
+    Takes care of the process of instantiation of arbitrary objects into camel case
+    properties recursively and also takes care of instantiating the respective
+    Argon model.
+    It only instantiates objects that contain the property _className hinting which
+    model is the one that corresponds to this object.
+    @property instantiate <public, static> [Function]
+    @argument element <required> [Object] {undefined} the object to instantiate
+    @return object
+    **/
     instantiate : function instantiate(element) {
         var property, i, result, className;
         
@@ -41,6 +65,12 @@ Class("Instantiator")({
         return result;
     },
 
+    /**
+    takes care of the property naming convention change
+    @property camelizeProperties <public, static> [Function]
+    @argument element <required> [Object]
+    @return object
+    **/
     camelizeProperties : function camelizeProperties(element) {
         var properties = {};
         var keyName;
